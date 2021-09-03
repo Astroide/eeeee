ba4132cabd176144f31f941208acf0da9320d483
Olie Auger <olie.auger@gmail.com>
2021-09-03 17:44:38 -0400

    Add warn() to utilities

--- compiler/src/utilities.ts
+++ compiler/src/utilities.ts
@@ -1,8 +1,11 @@
 import { exit } from "process";
 export function panic(message: string): never {
-    console.error('Fatal error: ' + message + '\nRun escurieux -h or escurieux --help for help.');
+    console.error('\u001b[31mFatal error: ' + message + '\nRun escurieux -h or escurieux --help for help.\u001b[0m');
     exit(1);
 }
 export function print(message: string) {
     console.error(message);
+}
+export function warn(message: string) {
+    console.error(`\u001b[33mWarning: ${message}\u001b[0m`);
 }
