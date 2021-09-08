eeadd371d8419b2b2d549907dcff0de0e43b3139
Olie Auger <olie.auger@gmail.com>
2021-09-08 19:42:57 -0400

    Add docs generator

---
+++ docs-generator.py
@@ -0,0 +1,29 @@
+import os
+files = []
+name_list = os.listdir('./docs-source')
+
+
+def generator():
+    i = 0
+    while i < len(name_list):
+        yield name_list[i]
+        i += 1
+
+
+for name in generator():
+    if os.path.isdir('./docs-source/' + name):
+        name_list.extend(
+            map(lambda n: name + '/' + n, os.listdir('./docs-source/' + name)))
+    else:
+        files.append(name)
+
+for file in files:
+    content = open('./docs-source/' + file, mode='r').read()
+    # process the content...
+    # end processing
+    file = os.path.splitext(file)[0]
+    os.makedirs(os.path.dirname(
+        './docs/' + file), exist_ok=True)
+    handle = open('./docs/' + file + '.html', mode='w')
+    handle.write(content)
+    handle.close()
