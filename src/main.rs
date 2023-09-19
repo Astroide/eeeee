113d5cb55dd498efb5dd41f388901032243237a1
astroide <olie.auger@gmail.com>
2023-09-19 17:56:38 -0400

    Ee ee e

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::path::PathBuf;
 
 fn main() {
-    let mut loader = escurieux::loader::Loader::new();
+    let mut loader = eeeee::loader::Loader::new();
     let index = loader.load_file(PathBuf::from("testing.txt")).unwrap();
-    let res = escurieux::lexer::lex(loader.get_file(index));
+    let res = eeeee::lexer::lex(loader.get_file(index));
     // eprintln!("{:?}", res.0);
     if let Err(errors) = res.1 {
         for error in errors.iter() {
-            escurieux::errors::print_error_(error, &loader);
-            // escurieux::errors::print_error(error, &loader)
+            eeeee::errors::print_error_(error, &loader);
+            // eeeee::errors::print_error(error, &loader)
         }
     }
-    escurieux::token_debugger::print_tokens(&res.0, &loader)
+    eeeee::token_debugger::print_tokens(&res.0, &loader)
 }
